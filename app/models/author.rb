1b264ff5bce8839022c41c4dcf3c1bcf4688f15d
Mark Viti <+>
2022-07-02 15:46:33 -0400

    commit tree fdd2c50ddac759c09f2e18b3efb2d699c17bda06

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+    has_many :posts
+
+    validates :name, presence: true
+    validates :email, uniqueness: true
+    validates :phone_number, length: {is: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+
+    validates :title, presence: true 
+    validates :content, length: { minimum: 100 }
+    validates :category , inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
 end
